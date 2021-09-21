c76a0c4bb024bf42e73fb653b9749aabed928c27
Warakorn Bunhlua <61160020@go.buu.ac.th>
2021-09-21 20:19:25 +0700

    Get Users 2

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -3,6 +3,6 @@ const app = require('../index')
 
 describe('GET /', () => {
     it('responds with "No user in the system"', (done) => {
-        request(app).get('/user').expect('No user in the system', done)
+        request(app).get('/users').expect('No user in the system', done)
     })
 })
